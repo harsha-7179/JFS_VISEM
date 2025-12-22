69b2e18a49329b71db0d26e97e1cdf7c98f0cb49
harsha_7179 <sreeharshah.2006@gmail.com>
2025-12-22 11:49:30 +0530

    hibernate employee

---
+++ MAVEN_PROJECTS/hibernate-employee/src/main/java/com/skillnext1/App.java
@@ -0,0 +1,26 @@
+package com.skillnext1;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class App {
+    public static void main(String[] args) {
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        Employee emp = new Employee(
+                "Harsha",
+                "harsha@gmail.com",
+                80000
+        );
+
+        session.persist(emp);
+
+        tx.commit();
+        session.close();
+
+        System.out.println("Employee inserted successfully!");
+    }
+}
+
